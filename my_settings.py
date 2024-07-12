d73dc3e239f7a8ed6c995dd009f2b3463aefbbd3
sujin-727 <sujin200507@gmail.com>
2024-07-13 01:43:29 +0900

    frist upload

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+MY_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'drama',
+        'USER': 'root',
+        'PASSWORD': '*deusujin3262', # 설정한 비밀번호로 적어주면 된다.
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+MY_SECRET_KEY = 'django-insecure-w(oo58ezhu)2x+zx!k+&w=-15w&q+za_b^c738vzb*do*mk$h@'
